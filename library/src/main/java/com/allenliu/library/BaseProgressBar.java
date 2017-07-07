dd13e1774fc84a4bd530c092bb678b526983be6c
allenliu <liusheng@fooyo.sg>
2017-07-07 17:16:18 +0800

    optimize

--- library/src/main/java/com/allenliu/library/BaseProgressBar.java
+++ library/src/main/java/com/allenliu/library/BaseProgressBar.java
@@ -2,6 +2,7 @@ package com.allenliu.library;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 
 
 /**
@@ -9,11 +10,19 @@ import android.content.Context;
  */
 
 public class BaseProgressBar extends Dialog {
-    public BaseProgressBar(Context context) {
+    public BaseProgressBar(final Context context) {
         super(context, R.style.BaseDialog);
         // TODO 自动生成的构造函数存根
         setContentView(R.layout.loading_progressbar_layout);
         setCanceledOnTouchOutside(false);
+        setOnCancelListener(new OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                if(context instanceof BaseActivity){
+                    ((BaseActivity) context).stopLoading();
+                }
+            }
+        });
     }
 
     @Override
